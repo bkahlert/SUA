ac8eda1a89b8242406f5cbc29f7eb6d1f339d85e
Kahlert, Björn <bjoern.kahlert@fu-berlin.de>
2013-01-17 18:54:10 +0100

    Detail Dialog for Screenshots now gives more information
    Former-commit-id: dfe193e15ca6fe85aaf64cb216dcb8cc2ca2eeb3

--- de.fu_berlin.imp.seqan.usability_analyzer.doclog/src/de/fu_berlin/imp/seqan/usability_analyzer/doclog/ui/widgets/DoclogDetailDialog.java
+++ de.fu_berlin.imp.seqan.usability_analyzer.doclog/src/de/fu_berlin/imp/seqan/usability_analyzer/doclog/ui/widgets/DoclogDetailDialog.java
@@ -1,9 +1,5 @@
 package de.fu_berlin.imp.seqan.usability_analyzer.doclog.ui.widgets;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
@@ -12,21 +8,28 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+import com.bkahlert.devel.nebula.widgets.RoundedComposite;
 
 import de.fu_berlin.imp.seqan.usability_analyzer.core.model.TimeZoneDateRange;
 import de.fu_berlin.imp.seqan.usability_analyzer.doclog.model.DoclogAction;
@@ -48,12 +51,25 @@ public class DoclogDetailDialog extends Dialog {
 	private DoclogRecord currentDoclogRecord;
 
 	private Label imageLabel;
+	private Link lblUrlvalue;
+	private Label lblIpvalue;
+	private Label lblPropxyipvalue;
+	private Label lblActionvalue;
+	private Label lblParamvalue;
+	private Label lblBoundsvalue;
+	private Label lblDatevalue;
+	private Label lblTimepassedvalue;
 
 	public DoclogDetailDialog(Shell parentShell, DoclogTimeline doclogTimeline,
 			DoclogRecord currentDoclogRecord) {
 		super(parentShell);
 		this.doclogTimeline = doclogTimeline;
 		this.currentDoclogRecord = currentDoclogRecord;
+	}
+
+	@Override
+	protected Point getInitialLocation(Point initialSize) {
+		return new Point(100, 70);
 	}
 
 	@Override
@@ -64,7 +80,75 @@ public class DoclogDetailDialog extends Dialog {
 
 	protected Control createDialogArea(Composite parent) {
 		Composite composite = (Composite) super.createDialogArea(parent);
-		composite.setLayout(new FillLayout());
+		composite.setBackgroundMode(SWT.INHERIT_DEFAULT);
+		GridLayout gl_composite = new GridLayout(2, false);
+		gl_composite.marginWidth = borderWidth;
+		gl_composite.marginHeight = borderWidth;
+		gl_composite.horizontalSpacing = borderWidth;
+		composite.setLayout(gl_composite);
+
+		Composite metaComposite = new RoundedComposite(composite, SWT.BORDER);
+		metaComposite.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_LIST_BACKGROUND));
+		metaComposite.setLayout(new GridLayout(2, false));
+		metaComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false,
+				false, 1, 1));
+
+		Label lblUrl = new Label(metaComposite, SWT.NONE);
+		lblUrl.setText("URL");
+
+		lblUrlvalue = new Link(metaComposite, SWT.NONE);
+		lblUrlvalue.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_LIST_BACKGROUND));
+		lblUrlvalue.setText("urlValue");
+		lblUrlvalue.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+				openURL(currentDoclogRecord.getUrl());
+			}
+		});
+
+		Label lblIp = new Label(metaComposite, SWT.NONE);
+		lblIp.setText("IP");
+
+		lblIpvalue = new Label(metaComposite, SWT.NONE);
+		lblIpvalue.setText("ipValue");
+
+		Label lblProxyip = new Label(metaComposite, SWT.NONE);
+		lblProxyip.setText("Proxy IP");
+
+		lblPropxyipvalue = new Label(metaComposite, SWT.NONE);
+		lblPropxyipvalue.setText("proxyipValue");
+
+		Label lblAction = new Label(metaComposite, SWT.NONE);
+		lblAction.setText("Action");
+
+		lblActionvalue = new Label(metaComposite, SWT.NONE);
+		lblActionvalue.setText("actionValue");
+
+		Label lblParam = new Label(metaComposite, SWT.NONE);
+		lblParam.setText("Param");
+
+		lblParamvalue = new Label(metaComposite, SWT.NONE);
+		lblParamvalue.setText("paramValue");
+
+		Label lblBounds = new Label(metaComposite, SWT.NONE);
+		lblBounds.setText("Bounds");
+
+		lblBoundsvalue = new Label(metaComposite, SWT.NONE);
+		lblBoundsvalue.setText("boundsValue");
+
+		Label lblDate = new Label(metaComposite, SWT.NONE);
+		lblDate.setText("Date");
+
+		lblDatevalue = new Label(metaComposite, SWT.NONE);
+		lblDatevalue.setText("dateValue");
+
+		Label lblTimePassed = new Label(metaComposite, SWT.NONE);
+		lblTimePassed.setText("Time Passed");
+
+		lblTimepassedvalue = new Label(metaComposite, SWT.NONE);
+		lblTimepassedvalue.setText("timePassedValue");
 
 		imageLabel = new Label(composite, SWT.NONE);
 		imageLabel.addListener(SWT.MouseUp, new Listener() {
@@ -106,6 +190,41 @@ public class DoclogDetailDialog extends Dialog {
 			return;
 		}
 
+		this.lblUrlvalue
+				.setText(doclogRecord.getShortUrl() != null ? doclogRecord
+						.getShortUrl() : "-");
+		this.lblIpvalue.setText(doclogRecord.getIp() != null ? doclogRecord
+				.getIp() : "-");
+		this.lblPropxyipvalue
+				.setText(doclogRecord.getProxyIp() != null ? doclogRecord
+						.getProxyIp() : "-");
+		this.lblActionvalue
+				.setText(doclogRecord.getAction() != null ? doclogRecord
+						.getAction().toString() : "-");
+		this.lblParamvalue
+				.setText(doclogRecord.getActionParameter() != null ? doclogRecord
+						.getActionParameter() : "-");
+		this.lblBoundsvalue
+				.setText((doclogRecord.getScrollPosition() != null ? doclogRecord
+						.getScrollPosition().x
+						+ ","
+						+ doclogRecord.getScrollPosition().y : "unknown")
+						+ " - "
+						+ (doclogRecord.getWindowDimensions() != null ? +doclogRecord
+								.getWindowDimensions().x
+								+ ","
+								+ doclogRecord.getWindowDimensions().y
+								: "unknown"));
+		this.lblDatevalue
+				.setText((doclogRecord.getDateRange() != null && doclogRecord
+						.getDateRange().getStartDate() != null) ? doclogRecord
+						.getDateRange().getStartDate().toISO8601() : "-");
+		;
+		this.lblTimepassedvalue
+				.setText((doclogRecord.getDateRange() != null && doclogRecord
+						.getDateRange().getDifference() != null) ? doclogRecord
+						.getDateRange().getDifference().toString() : "-");
+
 		disposeImage();
 
 		boolean isIntersected = false;
@@ -121,7 +240,8 @@ public class DoclogDetailDialog extends Dialog {
 		Color background = isIntersected ? COLOR_HIGHLIGHT : COLOR_STANDARD;
 		Image image = new Image(Display.getCurrent(), doclogRecord
 				.getScreenshot().getImageData());
-		if (doclogRecord.getAction() == DoclogAction.BLUR) {
+		if (doclogRecord.getAction() == DoclogAction.BLUR
+				|| doclogRecord.getAction() == DoclogAction.UNLOAD) {
 			GC gc = new GC(image);
 			gc.setBackground(background);
 			gc.setAlpha(70);
@@ -129,28 +249,18 @@ public class DoclogDetailDialog extends Dialog {
 			gc.dispose();
 		}
 
-		GC gc = new GC(image);
-		gc.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_BLACK));
-		gc.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
-		gc.setAlpha(200);
-		List<String> description = new ArrayList<String>();
-		description.add(doclogRecord.getUrl());
-		description.add(doclogRecord.getAction().toString());
-		description.add(doclogRecord.getActionParameter());
-		gc.drawText(StringUtils.join(description, "\n"), 10, 10, false);
-		gc.dispose();
-
 		imageLabel.setImage(image);
 		imageLabel.getParent().setBackground(background);
 
 		this.doclogTimeline.center(currentDoclogRecord.getDateRange());
 
+		((Composite) this.getContents()).layout();
 		Shell shell = this.getShell();
 		shell.pack();
-		Rectangle bounds = shell.getBounds();
-		int width = bounds.width + 2 * borderWidth;
-		int height = bounds.height + 2 * borderWidth;
-		shell.setBounds(bounds.x, bounds.y, width, height);
+		// Rectangle bounds = shell.getBounds();
+		// int width = bounds.width + 2 * borderWidth;
+		// int height = bounds.height + 2 * borderWidth;
+		// shell.setBounds(bounds.x, bounds.y, width, height);
 	}
 
 	private void disposeImage() {
@@ -181,7 +291,7 @@ public class DoclogDetailDialog extends Dialog {
 				.addSelectionListener(new SelectionAdapter() {
 					@Override
 					public void widgetSelected(SelectionEvent e) {
-						openURL();
+						openURL(currentDoclogRecord.getUrl());
 					}
 				});
 		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
@@ -206,10 +316,10 @@ public class DoclogDetailDialog extends Dialog {
 		}
 	}
 
-	public void openURL() {
-		if (currentDoclogRecord != null) {
-			org.eclipse.swt.program.Program
-					.launch(currentDoclogRecord.getUrl());
+	public static void openURL(String url) {
+		if (url != null) {
+			org.eclipse.swt.program.Program.launch(url);
 		}
 	}
+
 }
