ae16f4502c6f54252589907bf6ac510fb59a0ebe
Björn Kahlert <mail@bkahlert.com>
2014-02-28 05:53:18 +0100

    [FIX] Codes can now be highlighted in the CodeView through IHighlightService

--- de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory/src/de/fu_berlin/imp/seqan/usability_analyzer/groundedtheory/CodeLocatorProvider.java
+++ de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory/src/de/fu_berlin/imp/seqan/usability_analyzer/groundedtheory/CodeLocatorProvider.java
@@ -1,23 +1,30 @@
 package de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory;
 
 import java.net.URI;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.PlatformUI;
 
+import com.bkahlert.devel.rcp.selectionUtils.SelectionUtils;
 import com.bkahlert.nebula.utils.ExecUtils;
 
 import de.fu_berlin.imp.seqan.usability_analyzer.core.model.ILocatable;
 import de.fu_berlin.imp.seqan.usability_analyzer.core.services.location.AdaptingLocatorProvider;
+import de.fu_berlin.imp.seqan.usability_analyzer.core.util.WorkbenchUtils;
 import de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory.model.ICode;
 import de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory.services.ICodeService;
+import de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory.viewer.CodeViewer;
+import de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory.views.CodeView;
 
 public class CodeLocatorProvider extends AdaptingLocatorProvider {
 
 	public static final String CODE_NAMESPACE = "code";
 
+	@SuppressWarnings("unused")
 	private static final Logger LOGGER = Logger
 			.getLogger(CodeLocatorProvider.class);
 
@@ -66,33 +73,53 @@ public class CodeLocatorProvider extends AdaptingLocatorProvider {
 	public boolean showInWorkspace(final URI[] uris, boolean open,
 			IProgressMonitor monitor) {
 		if (uris.length > 0) {
-			try {
-				return ExecUtils.syncExec(new Callable<Boolean>() {
-					@Override
-					public Boolean call() throws Exception {
-						// EpisodeView episodeView = (EpisodeView)
-						// WorkbenchUtils
-						// .getView(EpisodeView.ID);
-						// if (episodeView == null) {
-						// return true;
-						// }
-						//
-						// EpisodeViewer viewer =
-						// episodeView.getEpisodeViewer();
-						// viewer.setSelection(new
-						// StructuredSelection(URIS));
-						// List<ILocatable> selected = SelectionUtils
-						// .getAdaptableObjects(viewer.getSelection(),
-						// ILocatable.class);
-						// return selected.size() == URIS.length;
-						return false;
-					}
-				});
-			} catch (Exception e) {
-				LOGGER.error(e);
-			}
+			CodeView codeView = (CodeView) WorkbenchUtils.getView(CodeView.ID);
+			return this.selectCodes(uris, codeView).length == uris.length;
+			// try {
+			// return ExecUtils.syncExec(new Callable<Boolean>() {
+			// @Override
+			// public Boolean call() throws Exception {
+			// EpisodeView episodeView = (EpisodeView)
+			// WorkbenchUtils
+			// .getView(EpisodeView.ID);
+			// if (episodeView == null) {
+			// return true;
+			// }
+			//
+			// EpisodeViewer viewer =
+			// episodeView.getEpisodeViewer();
+			// viewer.setSelection(new
+			// StructuredSelection(URIS));
+			// List<ILocatable> selected = SelectionUtils
+			// .getAdaptableObjects(viewer.getSelection(),
+			// ILocatable.class);
+			// return selected.size() == URIS.length;
+			// return false;
+			// }
+			// });
+			// } catch (Exception e) {
+			// LOGGER.error(e);
+			// }
 		}
 		return true;
+	}
+
+	private URI[] selectCodes(final URI[] uris, final CodeView codeView) {
+		try {
+			List<URI> selectedLocatables = ExecUtils
+					.syncExec(new Callable<List<URI>>() {
+						@Override
+						public List<URI> call() throws Exception {
+							final CodeViewer viewer = codeView.getCodeViewer();
+							viewer.setSelection(new StructuredSelection(uris));
+							return SelectionUtils.getAdaptableObjects(
+									viewer.getSelection(), URI.class);
+						}
+					});
+			return selectedLocatables.toArray(new URI[0]);
+		} catch (Exception e) {
+			return new URI[0];
+		}
 	}
 
 }
