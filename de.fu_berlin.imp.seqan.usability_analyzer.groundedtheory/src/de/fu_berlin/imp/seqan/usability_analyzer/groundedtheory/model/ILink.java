ca65d28a73787b44e0c17a34e74b2d3ea16a50a8
Kahlert, Björn <bjoern.kahlert@fu-berlin.de>
2014-05-13 17:25:42 +0200

    [INTERNAL] Default implementation of IEndpoint

--- de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory/src/de/fu_berlin/imp/seqan/usability_analyzer/groundedtheory/model/ILink.java
+++ de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory/src/de/fu_berlin/imp/seqan/usability_analyzer/groundedtheory/model/ILink.java
@@ -24,6 +24,109 @@ public interface ILink extends ILocatable {
 		public URI getCode();
 	}
 
+	public static class CoordinateEndpoint implements ICoordinateEndpoint {
+		private final int x;
+		private final int y;
+
+		public CoordinateEndpoint(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public long getX() {
+			return x;
+		}
+
+		@Override
+		public long getY() {
+			return y;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			CoordinateEndpoint other = (CoordinateEndpoint) obj;
+			if (x != other.x) {
+				return false;
+			}
+			if (y != other.y) {
+				return false;
+			}
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "{ " + x + ", " + y + " }";
+		}
+	}
+
+	public static class CodeEndpoint implements ICodeEndpoint {
+		private final URI id;
+
+		public CodeEndpoint(URI id) {
+			this.id = id;
+		}
+
+		@Override
+		public URI getCode() {
+			return id;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			CodeEndpoint other = (CodeEndpoint) obj;
+			if (id == null) {
+				if (other.id != null) {
+					return false;
+				}
+			} else if (!id.equals(other.id)) {
+				return false;
+			}
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "{ " + id + " }";
+		}
+	}
+
 	public String getTitle();
 
 	public IEndpoint getSource();
