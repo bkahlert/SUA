97bfdfd6e4f943882a400ea9fd7db4e204d56007
Kahlert, Björn <bjoern.kahlert@fu-berlin.de>
2014-06-10 14:45:37 +0200

    [FEATURE] Create top level code menu entry in CodeView

--- de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory/src/de/fu_berlin/imp/seqan/usability_analyzer/groundedtheory/handlers/CreateCodeHandler.java
+++ de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory/src/de/fu_berlin/imp/seqan/usability_analyzer/groundedtheory/handlers/CreateCodeHandler.java
@@ -1,11 +1,14 @@
 package de.fu_berlin.imp.seqan.usability_analyzer.groundedtheory.handlers;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.bkahlert.devel.rcp.selectionUtils.retriever.SelectionRetrieverFactory;
 
@@ -22,8 +25,11 @@ public class CreateCodeHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
-		List<ICode> codes = SelectionRetrieverFactory.getSelectionRetriever(
-				ICode.class).getSelection();
+		Collection<?> menus = HandlerUtil.getActiveMenus(event);
+
+		List<ICode> codes = menus.size() > 0 ? SelectionRetrieverFactory
+				.getSelectionRetriever(ICode.class).getSelection()
+				: new LinkedList<ICode>();
 		assert codes.size() < 2;
 
 		WizardUtils.openNewCodeWizard(codes.size() == 1 ? codes.get(0) : null,
