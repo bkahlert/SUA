f2947a23a7ff85c71573b2583ae25c27a5212a13
Björn Kahlert <mail@bkahlert.com>
2014-09-10 14:02:34 +0200

    no message

--- de.fu_berlin.imp.apiua.groundedtheory/src/de/fu_berlin/imp/apiua/groundedtheory/ui/DimensionValueComposite.java
+++ de.fu_berlin.imp.apiua.groundedtheory/src/de/fu_berlin/imp/apiua/groundedtheory/ui/DimensionValueComposite.java
@@ -207,6 +207,10 @@ public class DimensionValueComposite extends Composite {
 		}
 		this.valueEditors.clear();
 
+		if (this.isDisposed()) {
+			return;
+		}
+
 		if (this.dimensions.size() == 0) {
 			Label label = new Label(this, SWT.WRAP);
 			label.setText(EXPLANATION);
