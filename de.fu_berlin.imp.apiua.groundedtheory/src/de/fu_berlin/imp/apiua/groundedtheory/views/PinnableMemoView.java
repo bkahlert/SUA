ac390ef65bdcbb6afc6673dc2912d44b0c8357b6
Björn Kahlert <mail@bkahlert.com>
2014-11-27 06:50:50 +0100

    [NPE] Restoration of memo view delayed till data have been loaded (otherwise element could not have been loaded yet to retrieve the caption from)

--- de.fu_berlin.imp.apiua.groundedtheory/src/de/fu_berlin/imp/apiua/groundedtheory/views/PinnableMemoView.java
+++ de.fu_berlin.imp.apiua.groundedtheory/src/de/fu_berlin/imp/apiua/groundedtheory/views/PinnableMemoView.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.ISelectionListener;
-import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 
 import com.bkahlert.nebula.utils.ExecUtils;
 import com.bkahlert.nebula.utils.colors.RGB;
@@ -14,6 +13,9 @@ import com.bkahlert.nebula.utils.selection.SelectionUtils;
 import com.bkahlert.nebula.widgets.editor.Editor;
 
 import de.fu_berlin.imp.apiua.core.model.URI;
+import de.fu_berlin.imp.apiua.core.model.data.IBaseDataContainer;
+import de.fu_berlin.imp.apiua.core.services.DataServiceAdapter;
+import de.fu_berlin.imp.apiua.core.services.IDataService;
 import de.fu_berlin.imp.apiua.groundedtheory.preferences.SUAGTPreferenceUtil;
 
 public class PinnableMemoView extends AbstractMemoView {
@@ -24,65 +26,70 @@ public class PinnableMemoView extends AbstractMemoView {
 	private List<URI> lastLoadedObjects;
 	private List<URI> pinnedObjects = new LinkedList<URI>();
 
-	private final ISelectionListener selectionListener = new ISelectionListener() {
-		@Override
-		public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-			if (part == PinnableMemoView.this) {
-				return;
-			}
-
-			List<URI> toLoad = new ArrayList<URI>(
-					PinnableMemoView.this.pinnedObjects);
-			for (URI uri : SelectionUtils.getAdaptableObjects(selection,
-					URI.class)) {
-				if (!toLoad.contains(uri)) {
-					toLoad.add(uri);
-				}
-			}
-
-			PinnableMemoView.this.lastLoadedObjects = toLoad;
-			PinnableMemoView.this.load(toLoad);
+	private final ISelectionListener selectionListener = (part, selection) -> {
+		if (part == PinnableMemoView.this) {
+			return;
 		}
+
+		List<URI> toLoad = new ArrayList<URI>(
+				PinnableMemoView.this.pinnedObjects);
+		for (URI uri : SelectionUtils.getAdaptableObjects(selection, URI.class)) {
+			if (!toLoad.contains(uri)) {
+				toLoad.add(uri);
+			}
+		}
+
+		PinnableMemoView.this.lastLoadedObjects = toLoad;
+		PinnableMemoView.this.load(toLoad);
 	};
 
-	private final Runnable callback = new Runnable() {
-		@Override
-		public void run() {
-			for (Editor<URI> editor : PinnableMemoView.this.getEditors()) {
-				RGB rgb = editor.getBackgroundRGB();
-				if (rgb == null) {
-					rgb = RGB.WHITE;
-				}
-				if (PinnableMemoView.this.pinnedObjects.contains(editor
-						.getLoadedObject())) {
-					// TODO also mix colors of indirectly loaded code instances
-					editor.setBackground(pinnedBackgroundColor.mix(rgb, .5));
-				}
+	private final Runnable callback = () -> {
+		for (Editor<URI> editor : PinnableMemoView.this.getEditors()) {
+			RGB rgb = editor.getBackgroundRGB();
+			if (rgb == null) {
+				rgb = RGB.WHITE;
+			}
+			if (PinnableMemoView.this.pinnedObjects.contains(editor
+					.getLoadedObject())) {
+				// TODO also mix colors of indirectly loaded code instances
+				editor.setBackground(pinnedBackgroundColor.mix(rgb, .5));
 			}
 		}
 	};
 
 	public PinnableMemoView() {
+		IDataService dataService = (IDataService) PlatformUI.getWorkbench()
+				.getService(IDataService.class);
+		dataService.addDataServiceListener(new DataServiceAdapter() {
+			@Override
+			public void dataDirectoriesLoaded(
+					List<? extends IBaseDataContainer> dataContainers) {
+				PinnableMemoView.this.restore();
+			}
+		});
+	}
 
+	private boolean restored = false;
+
+	private void restore() {
+		if (this.restored) {
+			return;
+		}
+		this.restored = true;
+		List<URI> uris = new SUAGTPreferenceUtil().getLastOpenedMemos();
+		PinnableMemoView.this.loadAndClearHistory(null,
+				uris.toArray(new URI[0]));
+
+		ExecUtils.asyncExec(
+				() -> SelectionUtils.getSelectionService(
+						PinnableMemoView.this.getSite().getWorkbenchWindow())
+						.addPostSelectionListener(
+								PinnableMemoView.this.selectionListener), 1000);
 	}
 
 	@Override
 	public void postInit() {
 		super.postInit();
-
-		List<URI> uris = new SUAGTPreferenceUtil().getLastOpenedMemos();
-		PinnableMemoView.this.loadAndClearHistory(null,
-				uris.toArray(new URI[0]));
-
-		ExecUtils.asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				SelectionUtils.getSelectionService(
-						PinnableMemoView.this.getSite().getWorkbenchWindow())
-						.addPostSelectionListener(
-								PinnableMemoView.this.selectionListener);
-			}
-		}, 1000);
 	}
 
 	@Override
@@ -103,7 +110,7 @@ public class PinnableMemoView extends AbstractMemoView {
 	 * <p>
 	 * A pinned memo stays open although another object with memo support is
 	 * selected.
-	 * 
+	 *
 	 * @param pinnedObjects
 	 */
 	public void setPin(boolean pin) {
