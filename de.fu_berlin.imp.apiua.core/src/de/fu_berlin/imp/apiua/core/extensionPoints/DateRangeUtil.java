55921b0aba935282ffd473f2cde37563e96d0eeb
Björn Kahlert <mail@bkahlert.com>
2014-12-12 04:40:36 +0100

    [NPE]

--- de.fu_berlin.imp.apiua.core/src/de/fu_berlin/imp/apiua/core/extensionPoints/DateRangeUtil.java
+++ de.fu_berlin.imp.apiua.core/src/de/fu_berlin/imp/apiua/core/extensionPoints/DateRangeUtil.java
@@ -1,5 +1,6 @@
 package de.fu_berlin.imp.apiua.core.extensionPoints;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
@@ -7,10 +8,13 @@ import org.eclipse.core.runtime.Platform;
 import de.fu_berlin.imp.apiua.core.model.TimeZoneDateRange;
 
 public class DateRangeUtil {
-	public static void notifyDataSourceFilterChanged(TimeZoneDateRange oldDateRange,
-			TimeZoneDateRange newDateRange) {
-		IConfigurationElement[] config = Platform
-				.getExtensionRegistry()
+	public static void notifyDataSourceFilterChanged(
+			TimeZoneDateRange oldDateRange, TimeZoneDateRange newDateRange) {
+		if (ObjectUtils.equals(oldDateRange, newDateRange)) {
+			return;
+		}
+
+		IConfigurationElement[] config = Platform.getExtensionRegistry()
 				.getConfigurationElementsFor(
 						"de.fu_berlin.imp.apiua.core.daterange");
 		for (IConfigurationElement e : config) {
