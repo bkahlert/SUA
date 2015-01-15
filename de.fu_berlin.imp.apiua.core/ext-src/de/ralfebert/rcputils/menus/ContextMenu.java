3c67d37d83a45aefbab1c757e82b0dbd641d8d26
Björn Kahlert <mail@bkahlert.com>
2015-01-15 05:02:53 +0100

    [API] ContextMenu now also accepts Controls

--- de.fu_berlin.imp.apiua.core/ext-src/de/ralfebert/rcputils/menus/ContextMenu.java
+++ de.fu_berlin.imp.apiua.core/ext-src/de/ralfebert/rcputils/menus/ContextMenu.java
@@ -4,11 +4,12 @@ import org.apache.log4j.Logger;
 import org.eclipse.core.commands.common.CommandException;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
-import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.IWorkbenchActionConstants;
@@ -18,7 +19,7 @@ import org.eclipse.ui.handlers.IHandlerService;
 /**
  * ContextMenu creates a context menu for a structured viewer and registers it
  * with the workbench. It can assist with handling the default menu item.
- * 
+ *
  * @author Ralf Ebert
  * @see http://www.ralfebert.de/blog/eclipsercp/commands_context_menu/
  */
@@ -28,7 +29,7 @@ public abstract class ContextMenu {
 
 	private final Menu menu;
 	private final IWorkbenchPartSite site;
-	private final Viewer viewer;
+	private Viewer viewer;
 	private final boolean defaultItemHandling = false;
 
 	/**
@@ -37,24 +38,40 @@ public abstract class ContextMenu {
 	 * filled using contributions to "org.eclipse.ui.menus" with locationURI
 	 * "popup:<viewid>". If defaultItemHandling = true,
 	 */
-	public ContextMenu(Viewer viewer, final IWorkbenchPartSite site) {
-		this.viewer = viewer;
+	public ContextMenu(Control control, final IWorkbenchPartSite site) {
+		this.viewer = null;
 		this.site = site;
 
 		MenuManager menuManager = new MenuManager();
 		menuManager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 
-		if (viewer != null && viewer.getControl() != null) {
-			this.menu = menuManager.createContextMenu(viewer.getControl());
-			viewer.getControl().setMenu(this.menu);
+		if (control != null) {
+			this.menu = menuManager.createContextMenu(control);
+			control.setMenu(this.menu);
 		} else {
 			this.menu = null;
 		}
 
-		site.registerContextMenu(menuManager, viewer);
-		site.setSelectionProvider(viewer);
+		site.registerContextMenu(menuManager, this.viewer);
+		if (control instanceof ISelectionProvider) {
+			site.setSelectionProvider((ISelectionProvider) control);
+		}
 
 		this.setDefaultItemHandling(true);
+	}
+
+	/**
+	 * Creates an empty context menu for a structured viewer and registers it
+	 * for the given workbench part site. The created menu is intended to be
+	 * filled using contributions to "org.eclipse.ui.menus" with locationURI
+	 * "popup:<viewid>". If defaultItemHandling = true,
+	 */
+	public ContextMenu(Viewer viewer, final IWorkbenchPartSite site) {
+		this(viewer != null ? viewer.getControl() : null, site);
+		if (viewer != null) {
+			this.viewer = viewer;
+			site.setSelectionProvider(this.viewer);
+		}
 	}
 
 	/**
@@ -81,22 +98,18 @@ public abstract class ContextMenu {
 
 	protected abstract String getDefaultCommandID();
 
-	private final IDoubleClickListener listenerExecuteDefaultCommand = new IDoubleClickListener() {
-		@Override
-		public void doubleClick(DoubleClickEvent event) {
-			ContextMenu.this.menu.notifyListeners(SWT.Show, new Event());
-			IHandlerService handlerService = (IHandlerService) ContextMenu.this.site
-					.getService(IHandlerService.class);
-			try {
-				String defaultCommandID = ContextMenu.this
-						.getDefaultCommandID();
-				if (defaultCommandID != null) {
-					handlerService.executeCommand(
-							ContextMenu.this.getDefaultCommandID(), null);
-				}
-			} catch (CommandException e) {
-				LOGGER.error("Could not execute command", e);
+	private final IDoubleClickListener listenerExecuteDefaultCommand = event -> {
+		ContextMenu.this.menu.notifyListeners(SWT.Show, new Event());
+		IHandlerService handlerService = (IHandlerService) ContextMenu.this.site
+				.getService(IHandlerService.class);
+		try {
+			String defaultCommandID = ContextMenu.this.getDefaultCommandID();
+			if (defaultCommandID != null) {
+				handlerService.executeCommand(
+						ContextMenu.this.getDefaultCommandID(), null);
 			}
+		} catch (CommandException e) {
+			LOGGER.error("Could not execute command", e);
 		}
 	};
 
